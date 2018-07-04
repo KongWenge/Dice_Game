32faf496550801c0414d1abdcfec017c58090307
Yuki Homma <yuki_homma@wlan-napt-001.u-aizu.ac.jp>
2018-07-04 16:07:55 +0900

    Won/lost message added

--- dice_game.py
+++ dice_game.py
@@ -6,3 +6,8 @@ print("Die 1: %d" % dice1)
 dice2 = random.randint(1, 6)
 print("Die 2: %d" % dice2)
 print("Total value: %d" % (dice1 + dice2))
+if(dice1 + dice2) > 7:
+    print("You won!")
+else:
+    print("You lost")
+
